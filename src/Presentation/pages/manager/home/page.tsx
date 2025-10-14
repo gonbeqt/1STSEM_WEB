5de65ab7d2c7fec358d1536be2eb6f19b3d7aa19
c <charrymaedoria@gmail.com>
2025-10-14 18:05:33 +0800

    Update page.tsx

--- src/Presentation/pages/manager/home/page.tsx
+++ src/Presentation/pages/manager/home/page.tsx
@@ -317,7 +317,7 @@ const Home = observer(() => {
 
       <div className="w-full mx-auto px-4 sm:px-6 py-6">
          <div className="mb-8">
-          <h1 className="text-3xl font-bold text-gray-900 mb-2">Dashboard</h1>
+          <h1 className="text-3xl font-bold text-gray-900 mb-2">Home</h1>
         </div>
 
     
